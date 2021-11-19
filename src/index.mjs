4d129f4c40e7eea756e2e1c9ef81eb91a3258617
fynyky <iygnohil@gmail.com>
2021-11-19 02:48:05 +0000

    Initial style commit

--- src/index.mjs
+++ src/index.mjs
@@ -1,1 +1,17 @@
-console.log("Hello from index.mjs in Static Site Template");
+import { Loader } from "@googlemaps/js-api-loader";
+
+const loader = new Loader({
+    apiKey: "AIzaSyCVl-lqqBQvxKAKAOFzFB5Vd_BOjXtR-D0",
+    version: "weekly"
+});
+loader.load().then(() => {
+
+  let map = new google.maps.Map(document.getElementById("map"), {
+    center: { lat: 1.3521, lng: 103.8198 },
+    clickableIcons: false,
+    disableDefaultUI: true,
+    zoom: 12,
+  });
+
+});
+
